c03f1fbe71af3e49ad172c47bcdf9e2b6b292744
squat1side <squat1side@163.com>
2022-09-27 18:28:59 +0800

    Enemy_sound_attack

---
+++ Enemy_sound_attack.cpp
@@ -0,0 +1,177 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+#include "Components/SphereComponent.h"
+#include "AIController.h"
+#include "Main.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Components/BoxComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+
+
+
+// Sets default values
+AEnemy::AEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//将AgroSphere和CombatSphere作为子对象附加到根组件
+	AgroSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AgroSphere"));
+	AgroSphere->SetupAttachment(GetRootComponent());
+	AgroSphere->InitSphereRadius(500.f);
+
+	CombatSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CombatSphere"));
+	CombatSphere->SetupAttachment(GetRootComponent());
+	CombatSphere->InitSphereRadius(75.f);
+
+	//构建盒体碰撞函数并附加在根组件
+	CombatCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("CombatCollision"));
+	CombatCollision->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("EnemySocket"));
+
+	//bOverlappingCombatSphere
+	bOverlappingCombatSphere = false;
+
+	//设置Health，MaxHealth，Damage默认值
+	Health = 75.f;
+	MaxHealth = 100.f;
+	Damage = 10.f;
+}
+
+	// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+	{
+		Super::BeginPlay();
+
+		//获取 AI Controller的引用
+		AIController = Cast<AAIController>(GetController());
+
+		//设置重叠事件 -Agro/Combat的begin/End
+		AgroSphere->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::AgroSphereOnOverlapBegin);
+		AgroSphere->OnComponentEndOverlap.AddDynamic(this, &AEnemy::AgroSphereOnOverlapEnd);
+
+		CombatSphere->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::CombatSphereOnOverlapBegin);
+		CombatSphere->OnComponentEndOverlap.AddDynamic(this, &AEnemy::CombatSphereOnOverlapEnd);
+
+	}
+
+// Called every frame
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+//创建AgroSphereOnOverlapBegin函数
+void AEnemy::AgroSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		AMain* Main = Cast<AMain>(OtherActor);
+		if (Main)
+		{
+			MoveToTarget(Main);
+		}
+	}
+}
+
+//创建AgroSphereOnOverlapEnd函数
+void AEnemy::AgroSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor)
+	{
+		AMain* Main = Cast<AMain>(OtherActor);
+		{
+			if (Main)
+			{
+				SetEnemyMovementStatus(EEnemyMovementStatus::EMS_Idle);
+				if (AIController)
+				{
+					AIController->StopMovement();
+				}
+			}
+		}
+	}
+}
+
+//创建CombatSphereOnOverlapBegin函数
+void AEnemy::CombatSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		AMain* Main = Cast<AMain>(OtherActor);
+		{
+			if (Main)
+			{
+				CombatTarget = Main;
+				bOverlappingCombatSphere = true;
+				SetEnemyMovementStatus(EEnemyMovementStatus::EMS_Attacking);
+			}
+		}
+	}
+}
+
+//创建CombatSphereOnOverlapEnd函数
+void AEnemy::CombatSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor)
+	{
+		AMain* Main = Cast<AMain>(OtherActor);
+		{
+			if (Main)
+			{
+				bOverlappingCombatSphere = false;
+				if (EnemyMovementStatus != EEnemyMovementStatus::EMS_Attacking)
+				{
+					MoveToTarget(Main);
+					CombatTarget = nullptr;
+				}
+			}
+		}
+	}
+}
+
+//MoveToTarget函数
+void AEnemy::MoveToTarget(AMain* Target)
+{
+	SetEnemyMovementStatus(EEnemyMovementStatus::EMS_MoveToTarget);
+
+	if (AIController)
+	{
+		//创建参数FAIMoveRequest
+		FAIMoveRequest MoveRequest;
+		MoveRequest.SetGoalActor(Target);
+		//碰撞体之间的间隔
+		MoveRequest.SetAcceptanceRadius(10.0f);
+
+		//创建参数FNavPath
+		FNavPathSharedPtr NavPath;
+
+		//调用
+		AIController->MoveTo(MoveRequest, &NavPath);
+
+		/*
+				//NavPath -用于使用调试球来观察路径
+		auto PathPoints = NavPath->GetPathPoints();
+		for (auto Point : PathPoints)
+		{
+			FVector Location = Point.Location;
+
+			UKismetSystemLibrary::DrawDebugSphere(this, Location, 25.f, 8, FLinearColor::Red, 10.f, 1.5f);
+
+		}
+		*/
+	}
+}
+
+
+
